563df97a63ebf989a8b641a7e93de821f50ded38
MostafaEbrahim212 <sasaebrahim1@yahoo.com>
2024-04-22 20:46:46 +0200

    Update AdminController.php

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -17,8 +17,6 @@ class AdminController extends Controller
     }
     public function login(LoginRequest $request)
     {
-
-
         $validated = $request->validated();
         $admin = Admin::where('email', $validated['email'])->first();
         if (!$admin || !Hash::check($validated['password'], $admin->password)) {
@@ -29,9 +27,6 @@ class AdminController extends Controller
     }
     public function info(Request $request)
     {
-
-
-
         $admin = Auth::guard('admins')->user();
         return res_data($admin, 'Admin info', 200);
     }
